bd5e84250bcc47bedd55488f32fb5e0c999442a5
cipherjai <jaiatmart@gmail.com>
2016-09-22 01:59:33 +0530

    Variables

--- ex3.py
+++ ex3.py
@@ -21,4 +21,19 @@ print (3+2+1-5+4%2-1/4+6)
 print ("3+2<5-7")
 
 #TEsting with the Numbers
-print ()
+#Lets see how these are executed and in which order !
+#In the United States we use an acronym called PEMDAS which stands for
+#Parentheses Exponents Multiplication Division Addition Subtraction.
+#That's the order Python follows as well.
+
+
+# Now checking the presence of mind of snake!
+# We call it the snake fist in kung fu !
+
+print ("What is 3+2 ?", 3+2)
+print ("what is #3+2 ")
+
+#You know them
+#(abs)
+print (abs(3+2))
+3+2
