253fe6570f4f8f02cb98636a54a2724de084975f
cipherjai <jaiatmart@gmail.com>
2016-09-22 18:05:19 +0530

    Variable vs Numbers

--- ex4.py
+++ ex4.py
@@ -1,1 +1,27 @@
 # Variables
+cars = 100
+spaceInCar = 4.0
+drivers = 30
+passengers = 90
+carsNotDriven = cars - drivers
+print (carsNotDriven)
+# print {carsNotDri}
+SpaceInASportsCar = 42
+print (SpaceInASportsCar)
+# Therefore we can use Capital letters to name off a variavble
+# but we sould not
+
+print (SpaceInASportsCar*drivers)
+print (spaceInCar*drivers)
+# alpha vs Numbers
+
+print (cars)
+cars = "Mercedes"
+print (cars)
+
+#print (cars+100) not valid
+print (cars+'100')
+#merc100
+
+print (cars+"100")
+print (cars-"100")
