1ebff84b8d232eba9a07d4bf35562eb446250db4
cipherjai <jaiatmart@gmail.com>
2017-03-30 02:32:44 +0530

    Playing with the characters and lengths of strings Counting and enumerating

--- ex5Revision.py
+++ ex5Revision.py
@@ -32,4 +32,24 @@ print(7/4)
 print(round((1.499)))
 
 
+# Character Play
 
+mera_naam = "Jai Gupta"
+print(mera_naam[0])
+print(mera_naam[6])
+
+# computers start counting from 0
+# there is no negative zero ! Don't laugh but look at this
+
+print(mera_naam[-1])
+# therefore it prints the last character
+
+# ranging with the characters
+print(mera_naam[1:7])
+
+# ranging without beginnning
+print(mera_naam[:7])
+# print the whole fro starting
+
+# finding lengths
+print(len(mera_naam))
