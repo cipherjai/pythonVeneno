4b9064553977f69ad8902f6ab0cccc6f609acdd4
cipherjai <jaiatmart@gmail.com>
2017-03-31 01:34:16 +0530

    improved ex 25 and Debugged>Improved>Tested ex 26 for performance and issues

--- ex25.py
+++ ex25.py
@@ -27,7 +27,7 @@ def print_last_word(words):
 def sort_sentence(sentence):
     """Takes in a full sentence and returns the sorted words."""
     words = break_words(sentence)
-    return sort_words()
+    return sort_words(words)
 
 
 def print_first_and_last(sentence):
