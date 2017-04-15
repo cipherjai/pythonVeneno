5fd1e03643981d71119da94413e951a86a7ce695
cipherjai <jaiatmart@gmail.com>
2017-04-16 03:20:42 +0530

    basis for clases and strings , digged deeper into classes and further

---
+++ theNewBoston/strings_3.py
@@ -0,0 +1,15 @@
+# strings
+
+quote = "I don't think she's is 18."
+
+# using escape
+
+quote = 'I don\'t mind if she\'s drinking'
+print(quote)
+
+print(quote)
+
+firstName = "Jai"
+print(firstName + "Gupta")
+print(firstName, "Gupta")
+print((firstName+' ') * 5)
