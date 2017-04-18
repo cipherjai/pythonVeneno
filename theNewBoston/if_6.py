0ff540a0abef1b2f5f83dd9f0c073cbe98ec4fff
cipherjai <jaiatmart@gmail.com>
2017-04-19 03:13:33 +0530

    # if else work-up

---
+++ theNewBoston/if_6.py
@@ -0,0 +1,22 @@
+# if
+
+age = 23
+
+if age < 21:
+    print("No beer for you!")
+
+age = 20
+
+if age < 21:
+    print("No beer for you!")
+
+name = "Hillarious"
+
+if name is "Vaishnavi":
+    print("Hey there !")
+elif name is "Twinkle":
+    print("What up")
+elif name is "Twinkle Star":
+    print("How i wonder what you are ")
+else:
+    print("Well, Good try there !")
