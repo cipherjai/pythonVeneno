28d15759bf126a27be44a6d1e6c1b022cca54493
cipherjai <jaiatmart@gmail.com>
2018-01-07 01:25:44 +0530

    revised 1 -4

--- ex21.py
+++ ex21.py
@@ -30,6 +30,57 @@ iq = divide(100, 2)
 print("Age: %d, height: %d, Weight: %d, IQ: %d" % (age, height, weight, iq))
 
 
+# ==============================
+name = 23 + 43.0
+print("%d , She's crazy !" % name)
+# ==============================
+
+# ======= Anu Doubt ======
+print("============================")
+first_number = 10.25
+second_number = 7.5
+print(first_number - second_number)
+
+# error or something that is don't know
+x = 20.55
+y = 20.91
+z = 20.50
+
+print((x + y))
+print(x - z)
+
+# by jai
+floating_point_values = 0.5
+print ("%0.1f" % (floating_point_values))
+print ("%0.11f" % (floating_point_values))
+print ("%0.01f" % (floating_point_values))
+print ("%0.011f" % (floating_point_values))
+
+print("%0.1f" %(x - z))
+print("%0.11f" %(x - z))
+print("%0.01f" %(x - z))
+print(float(x - z))
+print()
+# Python stores floats with 'bits', and some floats you just can't represent accurately, no matter how many bits of
+# precision you have. This is the problem you have here. It's sorta like trying to write 1/3 in decimal with a limited
+#  amount of decimals places perfectly accurate.
+
+floating_point_values = 0.05
+print ("%0.1f" % (floating_point_values))
+print ("%0.11f" % (floating_point_values))
+print ("%0.01f" % (floating_point_values))
+print ("%0.011f" % (floating_point_values))
+print ("%0.00f" % (floating_point_values))
+print ("%0.001f" % (floating_point_values))
+print ("%0.010f" % (floating_point_values))
+
+
+# here's after all experiments
+print ("%0.2f" % (x - z))
+print ("%0.3f" % (x - z))
+
+print("============================")
+
 # Puzzle
 
 
--- ex3.py
+++ ex3.py
@@ -14,7 +14,7 @@ print("Hens ", 25 + 30 / 6)
 
 # modulus
 print(" Now I will count the eggs:")
-print(3+2+1-5 + 4 % 2 -1/4+6)
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
 
 print("3+2 < 5-7")
 
@@ -29,8 +29,8 @@ print("3+2 < 5-7")
 # Now checking the presence of mind of snake!
 # We call it the snake fist in kung fu !
 
-print ("What is 3+2 ?", 3+2)
-print ("what is #3+2 ")
+print("What is 3+2 ?", 3 + 2)
+print("what is #3+2 ")
 
 #You know them
 #(abs)
