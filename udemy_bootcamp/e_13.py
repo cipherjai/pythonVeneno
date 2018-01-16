e1b1eb8706994a15e83990da66edf57a77d232b6
cipherjai <jaiatmart@gmail.com>
2018-01-16 23:13:17 +0530

    revised 1 -4

---
+++ udemy_bootcamp/e_13.py
@@ -0,0 +1,8 @@
+# Numbers in Python
+# 3 types of numbers are present in python
+"""
+ 1. Integers 2 , -2
+ 2. Floating point, decimal -> 2.0, -2.5
+ 3. Exponent E -> 4E2 . Means 4 times 10 to the power of 2
+"""
+
