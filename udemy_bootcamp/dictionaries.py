c891358210580b3121b733bfc86c35fb5a5b372c
cipherJai <jaiatmart@gmail.com>
2018-07-11 20:59:37 +0530

    Dictionaries in python

---
+++ udemy_bootcamp/dictionaries.py
@@ -0,0 +1,23 @@
+# key and a value pair
+my_dict = {'key1':'value_1', 'key2':'value2'}
+print(my_dict['key1'])
+print(my_dict)
+print(type(my_dict))
+print(type(my_dict['key2']))
+my_dict = {'k1':123, 'k2':3.4, 'k3':'string'}
+print(my_dict['k3'][0])
+print(my_dict['k3'][-1])
+print(my_dict['k3'][::-1])
+print(my_dict['k3'].upper())
+print(my_dict['k3'])
+print(type(my_dict['k3']))
+
+my_dict['animal'] = 'Dog'
+my_dict['answer'] = '42'
+
+d = {'k1': {'nestkey': {'subnestkey': 'value'}}}
+print(d['k1']['nestkey']['subnestkey'])
+print((d['k1']['nestkey']['subnestkey']).upper())
+print(d.keys())
+print(d.values())
+print(d.items())
