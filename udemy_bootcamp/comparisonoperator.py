36d6d0f8f2e01cbbd1efb27537d3a90cf8ecac7b
cipherJai <jaiatmart@gmail.com>
2018-07-13 20:13:51 +0530

    chained comparison operator theory

---
+++ udemy_bootcamp/comparisonoperator.py
@@ -0,0 +1,14 @@
+print(type(2 != 1))
+
+# Chained comparison operators
+x = 3
+print(x>2 and x<5)
+print(2 < 3 < 5)
+print(2 < x and x > 5)
+
+start = 2
+end = 4
+if start <= x <= end:
+    print("all good")
+
+print(2 > 4 or 4 > 2)
