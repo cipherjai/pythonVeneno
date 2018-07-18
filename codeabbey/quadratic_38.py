6d6e9d2c60f0355dd0ed337982f72ee7704e05ee
cipherJai <jaiatmart@gmail.com>
2018-07-18 21:12:49 +0530

    codeabbey started and ranging function

---
+++ codeabbey/quadratic_38.py
@@ -0,0 +1,13 @@
+# a(x^2) + Bx + c = 0
+# import the math module
+# import math
+#
+# a = int(input("enter a > "));
+# b = int(input("enter b > "));
+# c = int(input("enter c > "));
+#
+# x1 = (-b + (math.sqrt(b^2 - 4*a*c))) / (2 * a);
+# x1 = (-b - (math.sqrt(b^2 - 4*a*c))) / (2 * a);
+#
+# print("x1 > ", x1);
+# print("x2 > ", x2);
---
+++ udemy_bootcamp/whileloop.py
@@ -0,0 +1,12 @@
+# while loop
+# this one has else
+
+# break continue pass
+x = 7
+while (x > 2):
+    print(x)
+    x -= 1
+else:
+    print("All Done!!")
+
+print(type(range(1, 10)))
