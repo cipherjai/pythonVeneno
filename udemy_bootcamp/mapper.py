f5980db87ac86c874b4bfb4151ad68dd2ef12685
cipherJai <jaiatmart@gmail.com>
2018-08-28 19:50:51 +0530

    Map and Reduce

--- udemy_bootcamp/mapper.py
+++ udemy_bootcamp/mapper.py
@@ -18,3 +18,10 @@ c = [7, 8, 9]
 
 print(list(map((lambda x,y,z : x+y+z), a, b, c)))
 print(list(map((lambda x, y, z: x**2+y**3+z**0.5), a, b, c)))
+
+
+# how to use tuples to be manipulated using lambda
+temps = [("Berlin", 29), ("Cairo", 36), ("Buenos Aires", 19), ("Los Angeles", 78.80), ("Tokyo", 27)]
+c_to_f = lambda data: (data[0], (9/5)*data[1] + 32)
+
+print(list(map(c_to_f, temps)))
--- udemy_bootcamp/reducer.py
+++ udemy_bootcamp/reducer.py
@@ -1,16 +1,11 @@
 # Use reduce to combine numbers
 from functools import reduce
-
-
 # we define a list of integers
 numbers = [1, 4, 6, 2, 9, 10]
 # Define a new function combine
 # Convert x and y to strings and create a tuple from x,y
 def combine(x,y):
   return "(" + str(x) + ", " + str(y) + ")"
-
-# Use reduce to apply combine to numbers
-from functools import reduce
 
 print(numbers)
 print(type(reduce(combine,numbers)))
@@ -23,3 +18,7 @@ numbers = [1, 4, 6, 2, 9, 10]
 
 print(numbers)
 print(reduce(lambda x,y: "(" + str(x) + ", " + str(y) + ")",numbers))
+
+list_of_randon_numbers = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12, 13], [5]]
+print(list(reduce(list.__add__, list_of_randon_numbers)))
+print(list(set(reduce(list.__add__, list_of_randon_numbers))))
