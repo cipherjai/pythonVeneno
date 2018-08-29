a10e7483030e4db66a8ca0e830bb6443e5cee2d1
cipherJai <jaiatmart@gmail.com>
2018-08-29 21:17:46 +0530

    Enumerate and map

---
+++ udemy_bootcamp/enumerater.py
@@ -0,0 +1,24 @@
+# enumerate the list items to refer them easily
+
+l = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+for count, obj in enumerate(l):
+    print (count, obj)
+
+# other uses of enum
+
+for count, obj in enumerate(l):
+    if count >= 2 :
+        break
+
+    else:
+        print(obj)
+
+# s = 'abcdefghijklmnopqrstuvwxyz'
+# x = ""
+# for i in s:
+#     x += ("'" + i + "', ")
+#
+# print(x)
+
+
