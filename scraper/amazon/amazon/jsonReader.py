05d7aa30e460ea8e3f74dd38c528b4ff163250eb
cipherJai <jaiatmart@gmail.com>
2018-09-05 19:02:41 +0530

    Added Spider Modification

--- scraper/amazon/amazon/jsonReader.py
+++ scraper/amazon/amazon/jsonReader.py
@@ -7,3 +7,12 @@ import json
 
 
 # checking latest f commit
+# Note that dump() takes two positional arguments: (1) the data object to be serialized, and (2) the file-like object to which the bytes will be written.
+
+# [list {dictionary -> (xyz : list[ ], )}]
+
+# Product name, details, most recent 100 reviews with review details
+with open("revw.json", encoding="utf-8-sig") as json_file:
+    text = json_file.read()
+    data = json.loads(text)
+    print(type(data))
--- scraper/amazon/amazon/spiders/AmazonReviewsSpider.py
+++ scraper/amazon/amazon/spiders/AmazonReviewsSpider.py
@@ -4,10 +4,12 @@ import scrapy
 class AmazonReviewsSpider(scrapy.Spider):
     name = 'AmazonReviews'
     allowed_domains = ['amazon.com']
-    start_urls = ["https://www.amazon.com/TCL-32S305-32-Inch-Smart-Model/product-reviews/B01MU1GBLL/ref=cm_cr_arp_d_viewopt_srt?ie=UTF8&reviewerType=all_reviews&pageNumber={}&sortBy=recent".format(i) for i in range(1,11)
+    start_urls = ["https://www.amazon.com/Amazon-Echo-Dot-Portable-Bluetooth-Speaker-with-Alexa-Black/dp/B01DFKC2SO?pd_rd_wg=jbHva&pd_rd_r=f997ef98-0614-42a3-acb5-bcb0d6eb7ecb&pd_rd_w=rZyMm&ref_=pd_gw_simh&pf_rd_r=TSMY899X7TSAPEE034AP&pf_rd_p=b841581f-e864-5164-afa6-4c18a8348879".format(i) for i in range(1,11)
      ]
 
     def parse(self, response):
+        prod_name =  response.xpath('//a[contains(@data-hook, "product-link")]/text()').extract()
+        prod_price = response.xpath('//span[contains(@class, "a-color-price arp-price")]/text()').extract()
         Revw_author = response.xpath('//a[contains(@data-hook, "review-author")]/text()').extract()
         Revw_title = response.xpath('//a[contains(@data-hook, "review-title")]/text()').extract()
         Revw_rating = response.xpath('//i[contains(@data-hook, "review-star-rating")]/span[contains(@class,"a-icon-alt")]/text()').extract()
@@ -16,11 +18,12 @@ class AmazonReviewsSpider(scrapy.Spider):
         Revw_helpful = response.xpath('//span[contains(@data-hook, "helpful-vote-statement")]/text()').extract()
 
         yield {
+            'prod_name' : prod_name,
+            'prod_price' : prod_price,
             'Review_author': Revw_author,
             'Review_title': Revw_title,
             'Review_rating': Revw_rating,
             'Review_date': Revw_date,
             'Review_content': Revw_content,
             'Review_helpfulness': Revw_helpful
-
         }
