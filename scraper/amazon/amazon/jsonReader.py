4c6c43a829cae2d2b3897479bb5e0f845a667677
cipherJai <jaiatmart@gmail.com>
2018-09-05 12:23:04 +0530

    checking files

---
+++ scraper/amazon/amazon/jsonReader.py
@@ -0,0 +1,9 @@
+import json
+
+# with open("revw.json", encoding = 'utf-8-sig') as json_file:
+#     text = json_file.read()
+#     json_data = json.loads(text)
+#     print(json_data)
+
+
+# checking latest f commit
